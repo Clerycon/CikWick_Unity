fdafa262e336092af95a6aa7e5a512fadcb96d61
Clerycon <clerycon2108@gmail.com>
2026-09-09 12:18:49 +0300

    v0.5 Player Jump

--- Assets/_GameAssets/Scripts/PlayerController.cs
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Reference")]
+    [Header("References")]
     [SerializeField] private Transform _orientation;
 
-    [Header("Value")]
+    private Rigidbody _rigidbody;
+    
+
+    [Header("Movement Settings")]
     [SerializeField] private float _movementSpeed;
 
-    private Rigidbody _rigidbody;
     private float _horizontalInput, _verticalInput;
     private Vector3 _movementDirection;
+
+
+    [Header("Jump Settings")]
+    [SerializeField] private KeyCode _jumpKey;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _jumpCooldown;
+    [SerializeField] private bool _canJump;
+
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private float _playerHeight;
+    [SerializeField] private LayerMask _groundLayer;
+     
     
     private void Awake()
     {
@@ -32,6 +47,13 @@ public class PlayerController : MonoBehaviour
     {
         _horizontalInput = Input.GetAxisRaw("Horizontal");
         _verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(_jumpKey) && _canJump && IsGrounded())
+        {
+            _canJump = false;
+            SetPlayerJump();
+            Invoke(nameof(ResetPlayerJump), _jumpCooldown);
+        }
     }
 
     private void SetPlayerMovement()
@@ -40,5 +62,21 @@ public class PlayerController : MonoBehaviour
             + _orientation.right * _horizontalInput).normalized;
         
         _rigidbody.AddForce(_movementDirection * _movementSpeed, ForceMode.Force);
+    }
+
+    private void SetPlayerJump()
+    {
+        _rigidbody.linearVelocity = new Vector3(_rigidbody.linearVelocity.x, 0f, _rigidbody.linearVelocity.z);
+        _rigidbody.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetPlayerJump()
+    {
+        _canJump = true;
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, _playerHeight * 0.5f + 0.2f, _groundLayer);
     }
 }
