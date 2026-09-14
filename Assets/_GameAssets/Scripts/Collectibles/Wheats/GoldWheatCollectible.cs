5b0a1dde3ab725a1649b176346fb0f0ccaffc275
Clerycon <clerycon2108@gmail.com>
2026-09-14 16:50:42 +0300

    v0.14 Wheat Design Scriptable Objects

--- Assets/_GameAssets/Scripts/Collectibles/Wheats/GoldWheatCollectible.cs
+++ Assets/_GameAssets/Scripts/Collectibles/Wheats/GoldWheatCollectible.cs
@@ -2,14 +2,12 @@ using UnityEngine;
 
 public class GoldWheatCollectible : MonoBehaviour, ICollectible
 {
+    [SerializeField] private WheatDesignSO _wheatDesignSO;
     [SerializeField] private PlayerController _playerController;
-
-    [SerializeField] private float _increaseBoostSpeed;
-    [SerializeField] private float _resetBoostDuration;
 
     public void Collect()
     {
-        _playerController.SetMovementSpeed(_increaseBoostSpeed, _resetBoostDuration);
+        _playerController.SetMovementSpeed(_wheatDesignSO.IncreaseDecreaseMultiplier, _wheatDesignSO.ResetBoostDuration);
         Destroy(gameObject);
     }
 }
