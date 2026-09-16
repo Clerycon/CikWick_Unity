a71215b42fd79d324c9d02ea2347e1c957d9dcc8
Clerycon <clerycon2108@gmail.com>
2026-09-16 15:49:34 +0300

    v0.20 Egg Counter UI

---
+++ Assets/_GameAssets/Scripts/UI/EggCounterUI.cs
@@ -0,0 +1,32 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class EggCounterUI : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private TMP_Text _eggCounterText; 
+
+    private RectTransform _eggCounterTransform;
+
+    [Header("Settings")]
+    [SerializeField] private Color _eggCounterColor;
+    [SerializeField] private float _colorDuration;
+    [SerializeField] private float _scaleDuration;
+
+    private void Awake()
+    {
+        _eggCounterTransform = _eggCounterText.gameObject.GetComponent<RectTransform>();
+    }
+
+    public void SetEggCounterText(int counter, int max)
+    {
+        _eggCounterText.text = counter.ToString() + "/" + max.ToString();
+    }
+
+    public void SetEggCompleted()
+    {
+        _eggCounterText.DOColor(_eggCounterColor, _colorDuration);
+        _eggCounterTransform.DOScale(1.2f, _scaleDuration).SetEase(Ease.OutBack);
+    }
+}
